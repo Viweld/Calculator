d0ee26de0fc5575fb6a0ff9a1f788592ebd68053
Viweld <litvar@outlook.com>
2020-11-30 15:44:13 +0400

    Начало использования системы контроля версий! Исходная конфигурация проекта.

---
+++ app/src/main/java/com/example/calc/KeyAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.calc
+
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.AbsListView
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+class KeyAdapter(private val mContext: Context,private val kW: Float,private val kH: Float) : BaseAdapter() {
+    override fun getCount(): Int {
+        return mThumbIds.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return mThumbIds[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val imageView: ImageView = ImageView(mContext)
+        imageView.layoutParams = AbsListView.LayoutParams((114.toFloat()*kW).toInt(), (106.toFloat()*kH).toInt())
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        imageView.setImageResource(mThumbIds[position])
+        return imageView
+    }
+
+    // references to our images
+    var mThumbIds = arrayOf<Int>(
+        R.drawable.b_7, R.drawable.b_8,R.drawable.b_9,R.drawable.b_ck,R.drawable.b_c,
+        R.drawable.b_4,R.drawable.b_5,R.drawable.b_6,R.drawable.b_um,R.drawable.b_del,
+        R.drawable.b_1,R.drawable.b_2,R.drawable.b_3,R.drawable.b_min,R.drawable.b_root,
+        R.drawable.b_0,R.drawable.b_dot,R.drawable.b_proc,R.drawable.b_plus,R.drawable.b_ravn
+
+
+    )
+}
+
---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.calc
+
+import android.content.Context
+import android.graphics.Typeface
+import android.os.*
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //П О Л О Ж Е Н И Е   К Н О П К И   В К Л Ю Ч Е Н И Я
+        im.post(Runnable {
+            //Измерение картинки
+            var HI = im.height.toFloat()
+            var WI = im.width.toFloat()
+            //Расчет коэффициентов сжатия если экран не по формату картинки
+            var kH: Float = HI / 1180.toFloat()
+            var kW: Float = WI / 720.toFloat()
+            Log.d("Tag", "$kW и $kH")
+            power.setX(0.toFloat() - 45.toFloat() * kW)
+            power.setY(360.toFloat() * kH)
+            power.scaleX = 0.5.toFloat() * kH * kH
+            power.scaleY = 0.5.toFloat() * kW
+        })
+
+        // В К Л Ю Ч Е Н И Е  /  В Ы К Л Ю Ч Е Н И Е
+        power.setOnCheckedChangeListener { buttonView, isChecked ->
+            //1. Вибрируем:
+ //           VIB()
+            if (isChecked == true) {
+                //2. Конфигурируем и активируем кнопочки
+                //Измерение картинки
+                var HI = im.height
+                var WI = im.width
+                //Расчет коэффициентов сжатия если экран не по формату картинки
+                var kH: Float = HI / 1180.toFloat()
+                var kW: Float = WI / 720.toFloat()
+
+                //Подстраиваем клавиатуру под картинку
+                keyboard.setPadding((67.toFloat() * kW).toInt(),
+                    (515.toFloat() * kH).toInt(),
+                    (67.toFloat() * kW).toInt(),
+                    0)
+                keyboard.horizontalSpacing = (5.toFloat() * kW).toInt()
+                keyboard.verticalSpacing = (5.toFloat() * kH).toInt()
+                //Приделываем кастомный адаптер к GridView под id:keyboard
+                val adapter = KeyAdapter(this, kW, kH)
+                keyboard.adapter = adapter
+                //3. отображаем НОЛИК на табло
+                tablo.text = "0"
+            } else {
+                tablo.text = ""
+            }
+        }
+
+
+        keyboard.setOnTouchListener { v, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN-> {
+                    //VIB()
+                    (v as ImageView).setImageResource(R.drawable.b_7_d)
+                }
+                MotionEvent.ACTION_UP-> {
+                        //VIB()
+                    (v as ImageView).setImageResource(R.drawable.b_7)
+                }
+                else -> {}
+            }
+            true
+        }
+
+
+        /*setOnItemClickListener { parent, view, position, id ->
+            if(position==0){
+                VIB()
+                (view as ImageView).setImageResource(R.drawable.b_7_d)
+                tablo.text="7"
+            }*/
+
+
+    }
+}
+
+
+//В И Б Р А Ц И Я   П Р И   Н А Ж А Т И И   К Н О П О К
+/*fun VIB() {
+    val vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    val canVibrate: Boolean = vibrator.hasVibrator()
+    val milliseconds = 70L
+    if (canVibrate) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // API 26
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    milliseconds,
+                    1
+                )
+            )
+        } else {
+            // This method was deprecated in API level 26
+            vibrator.vibrate(milliseconds)
+        }
+    }
+}*/
